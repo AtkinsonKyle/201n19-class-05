71cc078a123cda1475b3fc68faa605a4fa273d98
Kyle <kyleatkinson5@gmail.com>
2020-08-10 17:13:35 -0500

    finished product

--- starter-code/app.js
+++ starter-code/app.js
@@ -33,7 +33,7 @@ function multiply(a, b) { //eslint-disable-line
   var product = a * b;
   var string2 = `The product of ${a} and ${b} is ${product}.`;
   //console.log(product, string2);
-  return [product, string2]
+  return [product, string2];
 }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -87,7 +87,7 @@ function sumArray(sumArr) { //eslint-disable-line
   var two = sum(sumArr[2], one)[0];
   var string5 = `${testArray} was passed in as an array of numbers, and ${two} is their sum.`;
   //console.log(string5);
-  return[two, string5]
+  return[two, string5];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
@@ -105,20 +105,20 @@ Write a function called multiplyArray() that takes an array of numbers as its ar
 IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. This function should handle an array containing three elements. However, you may continue to use the + operator for string concatenation.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
-var testArray = [2, 3, 4];
+var testArray1 = [2, 3, 4];
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
   var one = multiply(multArr[0], multArr[1])[0];
   var two = multiply(multArr[2], one)[0];
-  var string5 = `The numbers ${testArray} have a product of ${two}.`;
+  var string5 = `The numbers ${testArray1} have a product of ${two}.`;
   console.log(string5);
-  return[two, string5]
+  return[two, string5];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-testMultiplyArray(testArray);
+//testMultiplyArray(testArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
 
